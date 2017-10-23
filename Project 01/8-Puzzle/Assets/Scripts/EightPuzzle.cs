08c71f14f1982201aabf80ac9b96cdc3e07edd7c
Jose Juan Sandoval <jose.j.sandoval@josejuansandoval.com>
2017-10-23 05:12:19 -0700

    Initial Push - Working on UI, Puzzle Solvable Done
    -Working on UI and animations.
    -Manual puzzle configuration entering is done.
    -Checking of inversions to see if puzzle is solvable is done.
    -Regular expressions to check if puzzle is valid is done.

---
+++ Project 01/8-Puzzle/Assets/Scripts/EightPuzzle.cs
@@ -0,0 +1,57 @@
+﻿using System.Text.RegularExpressions;
+using UnityEngine;
+
+public sealed class EightPuzzle
+{
+    private static readonly EightPuzzle instance = new EightPuzzle();
+    
+    private EightPuzzle() { }
+
+    public static EightPuzzle Instance
+    {
+        get
+        {
+            return instance;
+        }
+    }
+
+    public static bool IsPuzzleValid(string puzzleState)
+    {
+        Regex regex = new Regex("^[0-8]{9}$");
+
+        if (!regex.IsMatch(puzzleState))
+            return false;
+
+        return true;
+    }
+
+    public static bool IsSolvable(string puzzleState)
+    {    
+        return CountInversions(puzzleState) % 2 == 0;
+    }
+
+    public static int CountInversions(string puzzleState)
+    {
+        int inversions = 0;
+        int[] puzzleStateArray = new int[puzzleState.Length];
+
+        for(int i = 0; i < puzzleState.Length; i++)
+        {
+            puzzleStateArray[i] = int.Parse(puzzleState[i].ToString());
+        }
+
+        for(int i = 0; i < puzzleStateArray.Length; i++)
+        {
+            for(int j = i + 1; j < puzzleStateArray.Length; j++)
+            {
+                if (puzzleStateArray[i] > puzzleStateArray[j])
+                    inversions++;
+            }
+        }
+
+        Debug.Log("Inversions: " + inversions);
+
+        return inversions;
+    }
+    
+}
---
+++ Project 01/8-Puzzle/Assets/Scripts/GameManager.cs
@@ -0,0 +1,139 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class GameManager : MonoBehaviour
+{
+    public enum GameState
+    {
+        Playing,
+        Stop
+    }
+
+
+    public string puzzleState = string.Empty;
+    public float elapsedTime = 0.0f;
+    public GameState gameState = GameState.Stop;
+    public int moves = 0;
+
+    public GameObject emptyTile = null;
+
+    // Input Fields
+    [Header("Input Fields")]
+    public InputField puzzleStateInputField = null;
+
+    // Text
+    [Header("Text")]
+    public Text timer = null;
+    public Text movesCounter = null;
+
+    // Buttons
+    [Header("Buttons")]
+    public Button startButton = null;
+    public Button stopButton = null;
+
+    // Use this for initialization
+    void Start ()
+    {
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if(gameState == GameState.Playing)
+        {
+            elapsedTime += Time.deltaTime;
+            int minutes = Mathf.FloorToInt(elapsedTime / 60F);
+            int seconds = Mathf.FloorToInt(elapsedTime - minutes * 60);
+            timer.text = string.Format("{0:0}:{1:00}", minutes, seconds);
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 100))
+            {
+                if(!hit.transform.gameObject.name.Equals("Empty Tile"))
+                {
+                    moves++;
+                    movesCounter.text = moves.ToString();
+                    MoveTile(hit.transform.gameObject);
+                }
+                Debug.Log(hit.transform.gameObject.name);
+            }
+        }
+    }
+
+    public void SolveButtonClick()
+    {
+        //puzzleStateInputField = GameObject.Find("Puzzle State").GetComponent<InputField>();
+
+
+        if (EightPuzzle.IsPuzzleValid(puzzleStateInputField.text))
+        {
+            Debug.Log("Puzzle is valid!");
+
+            if (EightPuzzle.IsSolvable(puzzleStateInputField.text))
+                Debug.Log("Puzzle is solvable!");
+            else
+                Debug.Log("Puzzle is not solvable!");
+
+        }
+        else
+            Debug.Log("Puzzle is not valid!");
+
+    }
+
+    public void StartGame()
+    {
+        gameState = GameState.Playing;
+        startButton.gameObject.SetActive(false);
+        stopButton.gameObject.SetActive(true);
+    }
+
+    public void StopGame()
+    {
+        gameState = GameState.Stop;
+        startButton.gameObject.SetActive(true);
+        stopButton.gameObject.SetActive(false);
+    }
+
+    private void MoveTile(GameObject clickedTile)
+    {
+        Debug.Log("X: " + clickedTile.gameObject.transform.position.x
+            + " - Y: " + clickedTile.gameObject.transform.position.y);
+        Debug.Log("eX: " + emptyTile.gameObject.transform.position.x
+            + " - eY: " + emptyTile.gameObject.transform.position.y);
+        Debug.Log("Modified x: " + (clickedTile.gameObject.transform.position.x - 1.6).ToString());
+
+        if (emptyTile.transform.position.x == (clickedTile.gameObject.transform.position.x - 1.6)
+            && emptyTile.transform.position.y == clickedTile.gameObject.transform.position.y)
+        {
+            Debug.Log("X: " + clickedTile.gameObject.transform.position.x
+            + " - Y: " + clickedTile.gameObject.transform.position.y);
+            Debug.Log("Clicked tile is on the left.");
+        }
+        //else if (clickedTile.gameObject.transform.position.x - 1.6 == emptyTile.transform.position.x)
+        //{
+        //    Debug.Log("X: " + clickedTile.gameObject.transform.position.x
+        //    + " - Y: " + clickedTile.gameObject.transform.position.y);
+        //    Debug.Log("Clicked tile is on the right.");
+        //}
+        //else if (clickedTile.gameObject.transform.position.y - 1.6 == emptyTile.transform.position.y)
+        //{
+        //    Debug.Log("X: " + clickedTile.gameObject.transform.position.x
+        //    + " - Y: " + clickedTile.gameObject.transform.position.y);
+        //    Debug.Log("Clicked tile is above.");
+        //}
+        //else if (clickedTile.gameObject.transform.position.y - 1.6 == emptyTile.transform.position.y)
+        //{
+        //    Debug.Log("X: " + clickedTile.gameObject.transform.position.x
+        //    + " - Y: " + clickedTile.gameObject.transform.position.y);
+        //    Debug.Log("Clicked tile is below.");
+        //}
+    }
+}
